79c22c18afb10b35e6d3737dd5039b4ad668c23e
Zach Gilbert <gilberza@gmail.com>
2021-02-18 16:07:03 +0000

    Animated sliding of sticky header

--- js/stickyHeader.js
+++ js/stickyHeader.js
@@ -4,30 +4,36 @@ function createScrollSpy() {
   // Save processing by only executing on scroll direction change
   let lastDirection = null;
 
-  return $(window).scroll(function(event) {
+  return $(window).scroll(() => {
     let scrollTop = $(this).scrollTop();
 
     // Scrolls down
     if (scrollTop > lastScrollTop) {
 
       if (lastDirection !== 'down') {
-        $('#sticker').unstick();
-        lastDirection = 'down';
+        $('#sticker').addClass('slide-up');
+        $('#sticker').removeClass('slide-down');
+
+        setTimeout(() => {
+          $('#sticker').unstick();
+          lastDirection = 'down';
+        }, 300);
       }
     }
 
     // Scrolls up
-    else {
-      if (lastDirection !== 'up') {
-        // Show the header
-        $(document).ready(() => {
-          $('#sticker').sticky({
-            topSpacing: 0,
-            zIndex: 5000
-          });
+    else if (lastDirection !== 'up') {
+      // Show the header
+      $('#sticker').addClass('slide-down');
+      $('#sticker').removeClass('slide-up');
+
+      $(document).ready(() => {
+        $('#sticker').sticky({
+          topSpacing: 0,
+          zIndex: 5000
         });
-        lastDirection = 'up';
-      }
+      });
+      lastDirection = 'up';
     }
 
     lastScrollTop = scrollTop;
