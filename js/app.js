caf7e6e5443b31a39926300abc63bcda7840e15c
Zach Gilbert <gilberza@gmail.com>
2021-02-23 16:03:38 +0000

    Added slider position buttons and transferred styles from owl buttons classes. Removed owl buttons from project. Instantiated carousel in app.js. Removed references to Slick plugin.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
-
-checkAcceptCookies();
-
-const header = $('#sticker');
+// Cookies modal
+checkAcceptCookies();           // see js/cookies.js
 
 // Sticky header
-createScrollSpy(header);
+const header = $('#sticker');
+createScrollSpy(header);        // see js/stickyHeader.js
+
+// Carousel
+$(document).ready(function(){
+  $(".slider").simpleSlider({
+    interval: 4000,
+    animateDuration: 300
+  });
+});
