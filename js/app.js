033a9e75e539d8ec12613e10a35eec135d07429d
Zach Gilbert <gilberza@gmail.com>
2021-03-10 14:26:35 +0000

    Temporary workaround to fix scroll position

--- js/app.js
+++ js/app.js
@@ -12,3 +12,51 @@ $(document).ready(function(){
     animateDuration: 300
   });
 });
+
+class SideMenu {
+  constructor() {
+    this.scrollPosition = window.scrollY;
+  }
+  set scrollPosition(scrollPosition) {
+    this._scrollPosition = scrollPosition;
+  }
+  get scrollPosition() {
+    return this._scrollPosition;
+  }
+
+
+  showHideMobileNav() {
+    let page = $('#page-content');
+    page.toggleClass('mobile-nav-open');
+
+    // If closed, in the process of being opened
+    if (page.hasClass('mobile-nav-open')) {
+
+      // Call the setter method to set the current scroll position
+      this.scrollPosition = window.scrollY;
+
+      // Set styles to adjust page position
+      page.css({
+        'transform': 'translateX(-275px)',
+        'position': 'fixed',
+        'overflow': 'hidden',
+        'top': `-${this.scrollPosition}px`        // Keep position on page (otherwise jumpos to top)
+      });
+    }
+
+    // If open, in the process of being closed
+    else {
+      page.removeAttr('style');
+
+      setTimeout(() => {window.scroll(0, this.scrollPosition)}, 1);
+
+      createScrollSpy(header);
+    }
+  }
+}
+
+const sideNavHandler = new SideMenu();
+
+$('#hamburger-button').click(function () {
+  sideNavHandler.showHideMobileNav();
+});
