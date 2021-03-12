c3a0402ad888725c2f7c6cf9fce0f858f5b94c40
Zach Gilbert <gilberza@gmail.com>
2021-03-12 16:59:52 +0000

    Minor change for readability

--- js/app.js
+++ js/app.js
@@ -15,20 +15,22 @@ $(document).ready(function(){
   const sideNavHandler = new SideMenu();
 
   // Add a black alpha overlay to the DOM (set to hidden) for use in open/close
-  $('#page-content').append($('<div id="page-cover"></div>').css({
-    display: 'none',
-    position: 'fixed',
-    top: 0,
-    right: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: 'rgba(0,0,0,.5)',
-    cursor: 'pointer',
-    zIndex: 5001,
-    opacity: 0,
-    transitionPorperty: 'opacity',
-    transition: '0.5s'
-  }));
+  $('#page-content').append(
+    $('<div id="page-cover"></div>').css({
+      display: 'none',
+      position: 'fixed',
+      top: 0,
+      right: 0,
+      width: '100%',
+      height: '100%',
+      backgroundColor: 'rgba(0,0,0,.5)',
+      cursor: 'pointer',
+      zIndex: 5001,
+      opacity: 0,
+      transitionPorperty: 'opacity',
+      transition: '0.5s'
+    })
+  );
 
   const hamburgerButton = $('#hamburger-button');
   hamburgerButton.on('click', function(event) {
