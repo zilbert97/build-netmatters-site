51315aff6146071d99d771af77b05d7325919be3
Zach Gilbert <gilberza@gmail.com>
2021-04-22 13:29:23 +0100

    Contact form now submits to database

--- src/ContactForm.php
+++ src/ContactForm.php
@@ -149,14 +149,14 @@ class ContactForm extends SubmitForm
         $this->_setResults(
             [
                 'name'=>$this->getValueOnField('name_contact', 513),
-                'email'=>$this->getValueOnField('email_contact', 517),
-                'phone'=>$this->getValueOnField('phone_contact', 519),
+                'email_address'=>$this->getValueOnField('email_contact', 517),
+                'contact_number'=>$this->getValueOnField('phone_contact', 519),
                 'message'=>$this->getValueOnField('message_contact', 513),
                 'gdpr'=>$this->getValueOnField('accept_terms_contact', 513),
             ]
         );
 
-        $fieldsNotRequiredText = ['phone', 'gdpr'];
+        $fieldsNotRequiredText = ['contact_number', 'gdpr'];
 
         global $session;
 
@@ -169,7 +169,7 @@ class ContactForm extends SubmitForm
             $contactFormValuesBag->set($fieldName, $value);
         }
 
-        // Validate required fields not empty
+        // Validate required fields are not empty
         foreach ($this->_getResults() as $field => $result) {
             // Skip input fields that do not have * required marker
             // Technically GDPR is required but it is validated elsewhere
@@ -189,13 +189,17 @@ class ContactForm extends SubmitForm
             }
         }
 
+        // Get results and perform specific field validation on each
         $resultsValidated = [
-            $this->validateName($this->_getResults()['name']),
-            $this->validateEmail($this->_getResults()['email']),
-            $this->validatePhone($this->_getResults()['phone']),
-            $this->validateMessage($this->_getResults()['message']),
-            $this->validateGDPRAccepted($this->_getResults()['gdpr']),
+            'name'=>$this->validateName($this->_getResults()['name']),
+            'email_address'=>$this->validateEmail($this->_getResults()['email_address']),
+            'contact_number'=>$this->validatePhone($this->_getResults()['contact_number']),
+            'message'=>$this->validateMessage($this->_getResults()['message']),
+            'gdpr'=>$this->validateGDPRAccepted($this->_getResults()['gdpr']),
         ];
+
+        // If any validation returns a FormErrorMessage object, set error status
+        $hasErrors = false;
 
         foreach ($resultsValidated as $result) {
             if ($result instanceof FormErrorMessage) {
@@ -204,12 +208,71 @@ class ContactForm extends SubmitForm
             }
         }
 
-        // NTD - This needs to happen only if connected to and added to the database
+        // If no validation errors, attempt to submit (add to database)
         if (!$hasErrors) {
-            // Add success message to flash bag
-            $session->getFlashBag()->add('success', 'Your message was sent successfully!');
-            // Empty stored values from $contactFormValuesBag
-            $contactFormValuesBag->clear();
+
+            $db = connectToDatabase();
+
+            // If succesful connection to database, add row
+            if ($db) {
+
+                /* ============ SCHEMA ============
+                    CREATE TABLE contact (
+                        name TEXT NOT NULL,
+                        email_address TEXT NOT NULL,
+                        contact_number TEXT NOT NULL,
+                        message TEXT NOT NULL,
+                        submitted_at DATE NOT NULL
+                    );
+                ================================ */
+
+                $resultsToSubmit = array_merge(
+                    // Get values from validated results array that have keys
+                    array_intersect_key(
+                        $resultsValidated,
+                        array_flip(array('name', 'email_address', 'contact_number', 'message'))
+                    ),
+                    // Add current datetime to results to submit to database
+                    array('submitted_at'=>date('Y-m-d H:i:s'))
+                );
+
+                try {
+                    $query = 'INSERT INTO contact VALUES (
+                        :name, :email_address, :contact_number, :message, :submitted_at
+                    )';
+
+                    $stmt = $db->prepare($query);
+
+                    foreach ($resultsToSubmit as $key=>$val) {
+                        $stmt->bindValue(":$key", $val, PDO::PARAM_STR);
+                    }
+                    $stmt->execute();
+
+                    // Add success message to flash bag
+                    $session->getFlashBag()->add(
+                        'success',
+                        'Your message was sent successfully!'
+                    );
+
+                    // Empty stored values from $contactFormValuesBag
+                    $contactFormValuesBag->clear();
+
+                } catch (Exception $e) {
+                    // If unsucessful submit, add error to flashbag
+                    $session->getFlashBag()->add(
+                        'error',
+                        'Server error - failed to submit message'
+                    );
+                }
+
+            } else {
+                // If no connection to database, add error message to flash bag
+                // Note do not empty stored values from form so user can try again
+                $session->getFlashBag()->add(
+                    'error',
+                    'Server error - failed to submit message'
+                );
+            }
         }
 
         redirect('/contact.php');
